cc3e3cc5036508d733fec20bc3647c552d5767a5
SCKagura <110963025+SCKagura@users.noreply.github.com>
2024-02-23 12:07:14 +0700

    Update Lab02_Stepping_Motor.ino

--- Lab02_Stepping_Motor.ino
+++ Lab02_Stepping_Motor.ino
@@ -2,31 +2,68 @@
 #define input2 19
 #define input3 22
 #define input4 23
-void setup()
-{
-  Serial.begin(9600) ;
+
+void setup() {
+  Serial.begin(9600);
   pinMode(input1, OUTPUT);
   pinMode(input2, OUTPUT);
   pinMode(input3, OUTPUT);
   pinMode(input4, OUTPUT);
- 
 }
 
-void loop()
-{
+void turnClockWise() {
   // หมุนตามเข็มนาฬิกา
   digitalWrite(input1, HIGH);
   digitalWrite(input2, LOW);
   digitalWrite(input3, LOW);
   digitalWrite(input4, LOW);
-  delay(5); 
-  digitalWrite(input1,LOW);
- digitalWrite(input2, HIGH);
- delay(5) ;
- digitalWrite(input2,LOW);
- digitalWrite(input3, HIGH);
- delay(5) ;
- digitalWrite(input3,LOW);
- digitalWrite(input4, HIGH);
- delay(5) ;
+  delay(5);
+
+  digitalWrite(input1, LOW);
+  digitalWrite(input2, HIGH);
+  delay(5);
+
+  digitalWrite(input2, LOW);
+  digitalWrite(input3, HIGH);
+  delay(5);
+
+  digitalWrite(input3, LOW);
+  digitalWrite(input4, HIGH);
+  delay(5);
+}
+
+void turnCounterClockWise() {
+  digitalWrite(input1, LOW);
+  digitalWrite(input2, LOW);
+  digitalWrite(input3, LOW);
+  digitalWrite(input4, HIGH);
+  delay(5);
+
+  digitalWrite(input4, LOW);
+  digitalWrite(input3, HIGH);
+  delay(5);
+
+  digitalWrite(input3, LOW);
+  digitalWrite(input2, HIGH);
+
+  delay(5);
+
+  digitalWrite(input2, LOW);
+  digitalWrite(input1, HIGH);
+
+  delay(5);
+}
+
+void loop() {
+  unsigned long startTime = millis();  // เก็บเวลาเริ่มต้น
+
+  while (millis() - startTime <= 5000) {
+    turnClockWise();
+  }
+
+  startTime = millis();  // เก็บเวลาเริ่มต้นใหม่
+
+  while (millis() - startTime <= 5000) {
+    turnCounterClockWise();
+  }
 }
